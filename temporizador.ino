1bed6b17ef14d98a92ca3d26e42a24bc99957c64
Dember1998 <demberneptalyramosramirez@yahoo.es>
2018-06-23 18:24:13 -0600

    enfoque

--- temporizador.ino
+++ temporizador.ino
@@ -2,11 +2,18 @@
 
 class Reloj
 {
-private:
+public:
+  Reloj() {}
+  Reloj(int Horas, int Minutos, int Segundos)
+  {
+    horas = Horas;
+    minutos = Minutos;
+    segundos = Segundos;
+  }
+
   int horas = 0;
   int minutos = 0;
   int segundos = 0;
-  
 };
 
 class CuentaRegresiva : Reloj
@@ -17,10 +24,10 @@ public:
     horas = reloj.horas;
     minutos = reloj.minutos;
     segundos = reloj.segundos;
-  };
+  }
 
-// cuanta hacia atras y devuelve el resultado
-  void iniciar()
+  // cuanta hacia atras y devuelve el resultado
+  Reloj iniciar()
   {
     if (segundos < 0)
     {
@@ -35,10 +42,15 @@ public:
     }
     if (horas < 0)
       horas = 0;
-  };
+    Reloj r(horas, minutos, segundos);
+    return r;
+  }
 };
+Reloj r(1, 2, 3);
+Reloj temporizadores[] = {r};
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
 bool pinLed = LOW;
 void setup()
 {
