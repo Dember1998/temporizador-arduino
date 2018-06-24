c437d5d4a768098dde78b111e196dd06044ea657
Dember1998 <demberneptalyramosramirez@yahoo.es>
2018-06-23 18:04:33 -0600

    creacin clase cuentaRegresiva

--- temporizador.ino
+++ temporizador.ino
@@ -2,12 +2,41 @@
 
 class Reloj
 {
-public:
+private:
   int horas = 0;
   int minutos = 0;
   int segundos = 0;
+  
 };
 
+class CuentaRegresiva : Reloj
+{
+public:
+  CuentaRegresiva(Reloj reloj)
+  {
+    horas = reloj.horas;
+    minutos = reloj.minutos;
+    segundos = reloj.segundos;
+  };
+
+// cuanta hacia atras y devuelve el resultado
+  void iniciar()
+  {
+    if (segundos < 0)
+    {
+      minutos--;
+      segundos = 59;
+    }
+
+    if (minutos < 0)
+    {
+      horas--;
+      minutos = 59;
+    }
+    if (horas < 0)
+      horas = 0;
+  };
+};
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 bool pinLed = LOW;
